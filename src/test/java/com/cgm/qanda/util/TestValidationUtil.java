6e62a140757702cb32d3c86cbcaab79d7ce3fb00
ttalhattahir101 <ttalhattahir@gmail.com>
2020-05-23 21:29:27 +0500

    TestValidationUtil: Added new tests and improved existing ones

--- src/test/java/com/cgm/qanda/util/TestValidationUtil.java
+++ src/test/java/com/cgm/qanda/util/TestValidationUtil.java
@@ -16,30 +16,46 @@ import static org.junit.Assert.assertEquals;
         initializers = ConfigFileApplicationContextInitializer.class)
 public class TestValidationUtil {
     @Test
-    public void testValidateLength() {
+    public void testValidateCorrectLength() {
         String input = "test String";
         boolean validate = ValidationUtil.validateLength(input);
         assertEquals(true, validate);
     }
 
     @Test
-    public void testValidateLengthFailed() {
+    public void testValidateLengthFailedWithNullValue() {
         String input = null;
         boolean validate = ValidationUtil.validateLength(input);
         assertEquals(false, validate);
     }
 
     @Test
-    public void testValidateAnswerFormat() {
+    public void testValidateIncorrectAnswerFormatWithSingleQuotation() {
         String input = "this is input " + "\"" + "test";
-        boolean validate = ValidationUtil.validateAnswerFormat(input);
-        assertEquals(true, validate);
-    }
-
-    @Test
-    public void testValidateAnswerFormatFailure() {
-        String input = "this is wrong input";
         boolean validate = ValidationUtil.validateAnswerFormat(input);
         assertEquals(false, validate);
     }
+
+    @Test
+    public  void testValidateIncorrectAnswerFormatWithoutQuotation() {
+        String input1 = "this is wrong input";
+        boolean validate1 = ValidationUtil.validateAnswerFormat(input1);
+        assertEquals(false, validate1);
+    }
+
+    @Test
+    public void testValidateLengthWithIncorrectValue() {
+        String input =  "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a " +
+                "piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, " +
+                "a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin " +
+                "words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in " +
+                "classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 " +
+                "and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, " +
+                "written in 45 BC. This book is a treatise on the theory of ethics, very popular during the " +
+                "Renaissance Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a " +
+                "piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock";
+        boolean validate = ValidationUtil.validateLength(input);
+        assertEquals(false, validate);
+    }
+
 }
